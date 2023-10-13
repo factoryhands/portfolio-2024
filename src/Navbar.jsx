8fc5180e70264e2b0b3f993e13b8a55752796fed
factoryhands <jenniferlspriggs@gmail.com>
2023-10-13 09:38:29 +0000

    Adding menu instead of bars to responsive menu

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -22,19 +22,9 @@ const Navbar = () => {
                 <div className="-mr-2 -my-2 md:hidden">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Open menu</span>
-                    <svg
-                      className="h-6 w-6"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
+                    <span>
+                      Menu
+                    </span>
                   </Popover.Button>
                 </div>
                 <Popover.Group as="nav" className="hidden md:flex space-x-10">
